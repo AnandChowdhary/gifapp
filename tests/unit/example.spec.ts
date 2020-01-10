06db123bf4561f8b16c4824420125f8af47660ae
Anand Chowdhary <mail@anandchowdhary.com>
2020-01-10 18:29:44 +0530

    :green_heart: Mock translation for test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -5,7 +5,8 @@ describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { msg },
+      mocks: { $t: () => {} }
     });
     expect(wrapper.text()).toMatch(msg);
   });
