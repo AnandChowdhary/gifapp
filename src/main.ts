b41e67b8e5ab7cdc7fd4cf310d61f4384da9879d
Anand Chowdhary <mail@anandchowdhary.com>
2020-01-10 18:17:59 +0530

    :sparkles: Add i18n to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
 import Vue from "vue";
+import VueI18n from "vue-i18n";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+// Translation files
+import en from "@/i18n/en";
+
 Vue.config.productionTip = false;
+Vue.use(VueI18n);
+
+const messages = {
+  en
+};
+const i18n = new VueI18n({
+  locale: "en",
+  messages
+});
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
