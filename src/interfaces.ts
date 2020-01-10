75e0e276bafa081dbd49a59a0532065bb89cfbe8
Anand Chowdhary <mail@anandchowdhary.com>
2020-01-10 23:21:52 +0530

    :label: Add result interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,53 @@
-export interface GIPHYItem {}
+export interface GIPHYImage {
+  height: number;
+  size: number;
+  url: string;
+  webp: string;
+  webp_size: number;
+  mp4: string;
+  mp4_size: number;
+  width: number;
+}
+
+export interface GIPHYItem {
+  type: string;
+  id: string;
+  url: string;
+  slug: string;
+  bitly_url: string;
+  embed_url: string;
+  username: string;
+  source: string;
+  title: string;
+  rating: string;
+  import_datetime: Date;
+  trending_datetime: Date;
+  images: {
+    downsized_large: GIPHYImage;
+    fixed_height_small_still: GIPHYImage;
+    original: GIPHYImage;
+    fixed_height_downsampled: GIPHYImage;
+    downsized_still: GIPHYImage;
+    fixed_height_still: GIPHYImage;
+    downsized_medium: GIPHYImage;
+    downsized: GIPHYImage;
+    preview_webp: GIPHYImage;
+    original_mp4: GIPHYImage;
+    fixed_height_small: GIPHYImage;
+    fixed_height: GIPHYImage;
+    downsized_small: GIPHYImage;
+    preview: GIPHYImage;
+    fixed_width_downsampled: GIPHYImage;
+    fixed_width_small_still: GIPHYImage;
+    fixed_width_small: GIPHYImage;
+    original_still: GIPHYImage;
+    fixed_width_still: GIPHYImage;
+    looping: GIPHYImage;
+    fixed_width: GIPHYImage;
+    preview_gif: GIPHYImage;
+    "480w_still": GIPHYImage;
+  };
+}
 
 export interface GIPHYResult {
   data: GIPHYItem[];
