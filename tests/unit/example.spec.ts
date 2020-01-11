07b420cd1c416df95cb115759cce005858ab291c
Anand Chowdhary <mail@anandchowdhary.com>
2020-01-11 16:51:38 +0530

    :white_check_mark: Remove HelloWorld test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,12 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-      mocks: { $t: () => {} }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+// describe("HelloWorld.vue", () => {
+//   it("renders props.msg when passed", () => {
+//     const msg = "new message";
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg },
+//       mocks: { $t: () => {} }
+//     });
+//     expect(wrapper.text()).toMatch(msg);
+//   });
+// });
